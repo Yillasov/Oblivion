725bb2f80a5a543728b6c752deacd3ecbac1abd3
Yessine <yessine@MacBook-Pro-de-Yessine.local>
2025-03-25 08:33:25 +0100

    CI/CD

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+project = 'Oblivion SDK'
+copyright = '2023, Yessine'
+author = 'Yessine'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'myst_parser',  # For Markdown support
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+
+# Enable markdown files
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
